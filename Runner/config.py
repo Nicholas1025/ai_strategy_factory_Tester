9521d429e6d0a5fc32371e51771c9595e6ed7890
Beethoven <87802283+Nicholas1025@users.noreply.github.com>
2025-04-04 17:52:34 +0800

    Update config.py

--- Runner/config.py
+++ Runner/config.py
@@ -1,6 +1,6 @@
 # config.py
 
-OPENAI_API_KEY = "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"  # <- Your API Key
+OPENAI_API_KEY = " "  # <- Your API Key
 MODEL_NAME = "gpt-3.5-turbo"   
 
 STRATEGY_FOLDER = "strategy_output"
